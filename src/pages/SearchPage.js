d1b1a8a3cbd4bad1e6ed15fedcc0880fb34b9478
Karl McGuire <karl@xba.io>
2020-02-27 21:13:41 -0500

    gh spa redirect

--- src/pages/SearchPage.js
+++ src/pages/SearchPage.js
@@ -2,24 +2,40 @@ import React from 'react'
 import Nav from '../Nav.js'
 import './SearchPage.css'
 
-
 class SearchBar extends React.Component {
-  _input: ?HTMLInputElement
+  constructor(props) {
+    super(props)
+    this.state = {value: ''};
+
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
 
   componentDidMount() {
-    this._input.focus()
+    this.input.focus() 
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value})
+  }
+
+  handleSubmit(event) {
+    event.preventDefault()
+    alert(this.state.value)
   }
 
   render() {
     return (
-      <form className="search">
+      <form className="search" onSubmit={this.handleSubmit}>
         <input 
           className="search__bar" 
           type="text" 
           placeholder="Item name..." 
           spellCheck="false" 
           tabIndex="1"
-          ref={c => (this._input = c)} 
+          value={this.state.value}
+          onChange={this.handleChange}
+          ref={(input) => {this.input = input}}
         />
         <input 
           className="search__btn" 
