66bdfd6767655ce403945c665e871fd64f544c22
Karl McGuire <karl@xba.io>
2020-03-02 17:15:42 -0500

    vue skeleton

---
+++ src/main.js
@@ -0,0 +1,13 @@
+import Vue from 'vue'
+import App from './App.vue'
+import { library } from "@fortawesome/fontawesome-svg-core"
+import { fas } from "@fortawesome/free-solid-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome"
+
+library.add(fas)
+Vue.component("fas", FontAwesomeIcon)
+Vue.config.productionTip = false
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
