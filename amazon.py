8008a9239513cc30aa9612413a6dd4bdb113be1e
Karl McGuire <karl@xba.io>
2020-03-31 10:57:06 -0400

    tor example

--- amazon.py
+++ amazon.py
@@ -5,14 +5,20 @@ import requests
 from selectorlib import Extractor
 import json
 
-
 e = Extractor.from_yaml_file("amazon.yml")
+url = "https://www.amazon.com/Simple-Mobile-Prepaid-Midnight-Carrier/dp/B07XSQT4Q3/"
 
-url = "https://www.amazon.com/Rolex-Datejust-Mechanical-Automatic-Certified/dp/B015MXYTAQ/"
+# create a new session with tor ip
+session = requests.session()
+session.proxies = {}
+session.proxies["http"] = "socks5h://localhost:9050"
+session.proxies["https"] = "socks5h://localhost:9050"
 
-r = requests.get(url, {
-    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246"
+# get amazon page using tor
+r = session.get(url, headers={
+    "user-agent": "Mozilla/5.0 (iPad; CPU OS 9_3_5 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Mobile/13G36"
 })
 
+# extract and print
 data = e.extract(r.text)
 print(json.dumps(data, indent=True))
