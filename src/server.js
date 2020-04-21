ae392650ee38ecbed7536dd9023c227b57f0fd43
Karl McGuire <karl@karlmcguire.com>
2020-04-21 18:10:14 -0400

    docker

--- src/server.js
+++ src/server.js
@@ -1,1 +1,13 @@
-console.log("hello world")
+import express from "express"
+
+const app = express()
+
+const PORT = 8080
+
+app.get("/", (req, res) => {
+  res.send("hello world")
+})
+
+app.listen(PORT, () => {
+  console.log("listening on :" + PORT)
+})
